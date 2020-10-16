6c5a7953814f26b1fe66fffb4c46a71c21fe6718
Fantom7393 <raphael.lapointe99@gmail.com>
2020-10-16 17:23:42 -0400

    Update ApisController.cs, WeatherController.cs, and 6 more files...

---
+++ MUSInovation/MUSInovation/Models/Period.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+
+namespace MUSInovation.Models
+{
+    [DataContract]
+    public class Period
+    {
+        [DataMember(Name = "number", IsRequired = false)]
+        public int Number { get; set; }
+
+        [DataMember(Name = "name", IsRequired = true)]
+        public string Name { get; set; }
+
+        [DataMember(Name = "startTime", IsRequired = false)]
+        public DateTime StartTime { get; set; }
+
+        [DataMember(Name = "endTime", IsRequired = false)]
+        public DateTime EndTime { get; set; }
+
+        [DataMember(Name = "isDayTime", IsRequired = true)]
+        public bool IsDayTime { get; set; }
+
+        [DataMember(Name = "temperature", IsRequired = true)]
+        public int Temperature { get; set; }
+
+        [DataMember(Name = "temperatureUnit", IsRequired = true)]
+        public string TemperatureUnit { get; set; }
+
+        [DataMember(Name = "temperatureTrend", IsRequired = false)]
+        public string TemperatureTrend { get; set; }
+
+        [DataMember(Name = "windSpeed", IsRequired = true)]
+        public string WindSpeed { get; set; }
+
+        [DataMember(Name = "windDirection", IsRequired = true)]
+        public string WindDirection { get; set; }
+
+        [DataMember(Name = "icon", IsRequired = true)]
+        public string Icon { get; set; }
+
+        [DataMember(Name = "shortForecast", IsRequired = true)]
+        public string ShortForecast { get; set; }
+
+        [DataMember(Name = "detailedForecast", IsRequired = true)]
+        public string DetailedForecast { get; set; }
+    }
+}
---
+++ MUSInovation/MUSInovation/Models/Properties.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+
+namespace MUSInovation.Models
+{
+    [DataContract]
+    public class Properties
+    {
+        [DataMember(Name = "updated", IsRequired = false)]
+        public DateTime Updated { get; set; }
+
+        [DataMember(Name = "units", IsRequired = false)]
+        public string Units { get; set; }
+
+        [DataMember(Name = "forecastGenerator", IsRequired = false)]
+        public string ForecastGenerator { get; set; }
+
+        [DataMember(Name = "generatedAt", IsRequired = false)]
+        public DateTime GeneratedAt { get; set; }
+
+        [DataMember(Name = "updateTime", IsRequired = false)]
+        public DateTime UpdateTime { get; set; }
+
+        [DataMember(Name = "validTime", IsRequired = false)]
+        public DateTime ValidTime { get; set; }
+
+        [DataMember(Name = "elevation", IsRequired = false)]
+        public Elevation Elevation { get; set; }
+
+        [DataMember(Name = "periods", IsRequired = true)]
+        public List<Period> Periods { get; set; }
+    }
+
+    [DataContract]
+    public class Elevation
+    {
+        [DataMember(Name = "value", IsRequired = false)]
+        public double Value { get; set; }
+
+        [DataMember(Name = "unitCode", IsRequired = false)]
+        public string UnitCode { get; set; }
+    }
+}
